00c2b6833bc556bfbca9a11200892b056b6e1c3b
glink25 <49364151+glink25@users.noreply.github.com>
2020-06-13 21:32:31 +0800

    details

--- sw.js
+++ sw.js
@@ -6,13 +6,11 @@
  * Register service worker.
  * ========================================================== */
 
-const PRECACHE = 'precache-v1';
+const PRECACHE = 'precache-v2';
 const RUNTIME = 'runtime';
 const HOSTNAME_WHITELIST = [
   self.location.hostname,
-  "huangxuan.me",
-  "yanshuo.io",
-  "cdnjs.cloudflare.com"
+  "glink25.github.io",
 ]
 
 
